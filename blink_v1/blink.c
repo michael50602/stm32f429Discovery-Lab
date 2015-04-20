ef48b6ef83f1889ad1aa8e44fb6c7ed5978909e5
Joou <luckyjoou@gmail.com>
2015-04-21 02:16:17 +0800

    add introduction to blink.c

--- blink_v1/blink.c
+++ blink_v1/blink.c
@@ -1,3 +1,15 @@
+/*
+ *	This program turns on/off LEDs on STM32F429 Discovery Board
+ *  
+ *  You can see the reference from embedded2015.hackpad.com/Week-7-8--ID8HJ3uW0MO
+ *
+ *	If you have any question, please Email to me
+ *
+ *	John Chou <luckyjoou@gmail.com>
+ *
+ */
+
+
 #define RCC_AHB1ENR		 (*((volatile unsigned long *) (0x40023800 + 0x30)))
 #define GPIOG_MODER		 (*((volatile unsigned long *) (0x40021800 + 0x00)))
 #define GPIOG_OTYPER  	(*((volatile unsigned long *) (0x40021800 + 0x04)))
@@ -11,7 +23,7 @@ int main()
 	unsigned int c=0;
 	RCC_AHB1ENR = (1 << 6);
 	GPIOG_MODER = 0x14000000;
-	//GPIOG_OTYPER &= ~(1 << 13);
+	//GPIOG_OTYPER &= ~(1 << 13);	//GPIOG_OTYPER is default push-pull
 	GPIOG_OSPEEDR = 0x14000000;
 	while(1){
 		GPIOG_BSRR = 0x40002000;	//Only PG13 ON
