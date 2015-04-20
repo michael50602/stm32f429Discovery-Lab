3a957d155d8dd698691e81b50119679b10a7605e
Joou <luckyjoou@gmail.com>
2015-04-21 02:05:34 +0800

    add blink_v1

---
+++ blink_v1/blink.c
@@ -0,0 +1,22 @@
+#define RCC_AHB1ENR		 (*((volatile unsigned long *) (0x40023800 + 0x30)))
+#define GPIOG_MODER		 (*((volatile unsigned long *) (0x40021800 + 0x00)))
+#define GPIOG_OTYPER  	(*((volatile unsigned long *) (0x40021800 + 0x04)))
+#define GPIOG_OSPEEDR 	(*((volatile unsigned long *) (0x40021800 + 0x08)))
+#define GPIOG_BSRR	 	(*((volatile unsigned long *) (0x40021800 + 0x18)))
+
+asm(".word 0x20001000");
+asm(".word main");
+int main()
+{
+	unsigned int c=0;
+	RCC_AHB1ENR = (1 << 6);
+	GPIOG_MODER = 0x14000000;
+	//GPIOG_OTYPER &= ~(1 << 13);
+	GPIOG_OSPEEDR = 0x14000000;
+	while(1){
+		GPIOG_BSRR = 0x40002000;	//Only PG13 ON
+		for(c=0;c<100000;c++);
+		GPIOG_BSRR = 0x20004000;	//Only PG14 ON
+		for(c=0;c<100000;c++);
+	}
+}
